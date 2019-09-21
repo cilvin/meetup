15ffd3f16a38cdb158bdac0e7655446f8ef1515d
cilvin <cilvin33@users.noreply.github.com>
2019-09-21 11:16:01 -0700

    Add test file and refredhAccessToken function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -15,6 +15,32 @@ module.exports.getAccessToken = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=jr2bsub23bropmv7e9cn3v9mvp'
+    + '&client_secret=j0jrlqsm7357f31q6n4a9lt2sd'
+    + '&grant_type=refresh_token'
+    + '&refresh_token=' + event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*"
+    },
     body: JSON.stringify({
       access_token: info.data.access_token,
       refresh_token: info.data.refresh_token,
