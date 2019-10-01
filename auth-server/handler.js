a7193d20889fdaa288d0f09f7925f1af4b527dae
cilvin <cilvin33@users.noreply.github.com>
2019-10-01 07:40:46 -0700

    4.3 First scenario

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -3,7 +3,8 @@
 const axios = require('axios');
 
 module.exports.getAccessToken = async (event) => {
-
+  
+  console.log(event.pathParameters,'test')
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
     + '?client_id=jr2bsub23bropmv7e9cn3v9mvp'
     + '&client_secret=j0jrlqsm7357f31q6n4a9lt2sd'
