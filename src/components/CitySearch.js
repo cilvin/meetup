ac41bafb42206e5278d2edf3eeedf2f574faf6dc
cilvin <cilvin33@users.noreply.github.com>
2019-10-17 12:47:06 -0700

    4.4 Update city search

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import { getSuggestions } from '../api';
 
-
 class CitySearch extends Component {
+  state = {
+    query: '',
+    suggestions: []
+  };
 
-  state ={
-    query:'',
-    suggestions: [],
-  }
-  
-  handleInputChanged = (event) => {
+  handleInputChanged = event => {
     const value = event.target.value;
     this.setState({ query: value });
     getSuggestions(value).then(suggestions => this.setState({ suggestions }));
-  }
+  };
 
-  handleItemClicked = (value) => {
-    this.setState({ query: value });
+  handleItemClicked = (value, lat, lon) => {
+    this.setState({ query: value, suggestions: [] });
+    this.props.updateEvents(lat, lon);
   }
-
   render() {
     return (
       <div className="CitySearch">
-        <input type="text" className="city" onChange={this.handleInputChanged} value={this.state.query} />
+        <input
+          type="text"
+          className="city"
+          onChange={this.handleInputChanged}
+          value={this.state.query}
+        />
         <ul className="suggestions">
-          {this.state.suggestions.map(item => 
-            <li onClick={() => this.handleItemClicked(item.name_string)}  key={item.name_string}>{item.name_string}</li>)}
+          {this.state.suggestions.map(item => (
+            <li
+              key={item.name_string}
+              onClick={() =>
+                this.handleItemClicked(item.name_string, item.lat, item.lon)
+              }
+            >
+              {item.name_string}
+            </li>
+          ))}
         </ul>
       </div>
     );
